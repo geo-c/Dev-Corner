7dce69875b9a015198fc9475f0e998c0de719de3
Nicholas Schiestel <nicho90@users.noreply.github.com>
2016-03-14 12:39:37 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,7 +12,7 @@ $( document ).ready(function() {
     };
 
     function insertLogo(url) {
-        $( '.wy-side-nav-search' ).append(
+        $( '.icon' ).html(
         	'<img src="' + url + '" style="width:auto; height:30px; border-radius:0px;">Dev-Corner'
       	);
     };
