2875bbb62d1acd7d7bbfff965ca6d384a22ad25f
Nicholas Schiestel <nicho90@users.noreply.github.com>
2016-03-14 13:05:33 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-    $('.wy-side-nav-search a).before(
+    $('.wy-side-nav-search a').before(
         '<img src="http://giv-oct.uni-muenster.de/dev-corner/img/logo-geoc_white.png" style="width:auto; height:30px; border-radius:0px;">'
     );
 
