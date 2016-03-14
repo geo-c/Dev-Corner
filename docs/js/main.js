ca96cd61d1c8821607c59326aa2d9bc6b827cbc6
Nicholas Schiestel <nicho90@users.noreply.github.com>
2016-03-14 13:19:17 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
     $('.wy-side-nav-search a').before(
-        '<img src="http://giv-oct.uni-muenster.de/dev-corner/img/logo-geoc_white.png" style="width:auto; height:30px; border-radius:0px;">'
+        '<a href="http://giv-oct.uni-muenster.de/dev-corner"><img src="http://giv-oct.uni-muenster.de/dev-corner/img/logo-geoc_white.png" style="width:auto; height:30px; border-radius:0px;"></a>'
     );
 
 });
