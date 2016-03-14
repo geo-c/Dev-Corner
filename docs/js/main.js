59d0c0ce2d6c8970102801373685831fc54750b3
Nicholas Schiestel <nicho90@users.noreply.github.com>
2016-03-14 13:02:06 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,22 +1,22 @@
 $( document ).ready(function() {
 
-    console.log(window.location.pathname);
-    console.log(window.location.href);
+    //console.log(window.location.pathname);
+    //console.log(window.location.href);
 
-    function urlExists() {
+    /*function urlExists() {
         $('<img src="' + window.location.pathname + 'img/logo-geoc_white.png">').load(function() {
             insertLogo(window.location.pathname + "/img/logo-geoc_white.png");
         }).bind('error', function() {
-            insertLogo("../../img/logo-geoc_white.png");
+            insertLogo("http://giv-oct.uni-muenster.de/dev-corner/img/logo-geoc_white.png");
         });
     };
 
-    function insertLogo(url) {
-        $('div[role="search"]').before(
-            '<img src="' + url + '" style="width:auto; height:30px; border-radius:0px;">'  
+    function insertLogo(url) {*/
+        $('a[href="."]').before(
+            '<img src="http://giv-oct.uni-muenster.de/dev-corner/img/logo-geoc_white.png" style="width:auto; height:30px; border-radius:0px;">'  
         );
-    };
+    //};
 
-    urlExists();
+    //urlExists();
 
 });
